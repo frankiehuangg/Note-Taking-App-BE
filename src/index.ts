9b916b5e748daaa52e95402d3aa1c6747c6ad578
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-03-23 22:14:26 +0700

    feat: setup the main process

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
-const app = express()
-const port = 5000
+import app from './router/router'
+import constants from "./constants/constants";
+import mongoose from "mongoose";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Configure server
+const server = express()
 
-app.listen(port, () => {
-  console.log(`Express is listening at http://localhost:${port}`)
+// Header information
+server.use(cors())
+server.disable('x-powered-by')
+server.use(cookieParser())
+server.use(express.urlencoded({extended: false}))
+server.use(express.json())
+
+// Start Mongoose
+console.log(constants.MONGODB_URI)
+mongoose
+    .connect(constants.MONGODB_URI)
+    .then(() => console.log(`Connected to database`))
+    .catch((err) => console.log(err))
+
+// Configure routes
+server.use(app)
+
+// Start server
+server.listen(constants.BE_PORT, () => {
+    console.log(`Server running on http://localhost:${constants.BE_PORT}`)
 })
