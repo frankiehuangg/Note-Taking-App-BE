90b65c20c9bbc5e7727fc4777dee29f6d78f2589
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-03-24 11:57:13 +0700

    feat: implement the Note dto model

---
+++ src/models/dto/note.ts
@@ -0,0 +1,37 @@
+export interface PostCreateNoteRequest {
+    userID: string,
+    title: string,
+    content: string,
+    type: string
+}
+
+export interface Note {
+    modified_at: string,
+    id: string,
+    title: string,
+    content: string,
+    type: string
+}
+
+export interface PostCreateNoteResponse {
+    note: Note
+}
+
+export interface GetRetrieveNoteResponse {
+    notes: Note[]
+}
+
+export interface PatchUpdateNoteRequest {
+    id: string,
+    title: string,
+    content: string,
+    type: string
+}
+
+export interface PatchUpdateNoteResponse {
+    note: Note
+}
+
+export interface DeleteRemoveNoteRequest {
+    id: string
+}
